20dce98421511e4c54d37cb03c06b58d13508361
Adarsh <adarshniharika20@gmail.com>
2026-06-02 12:43:50 +0530

    Add project files.

---
+++ DataBaseLayer/Context/UserDbContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModelLayer.Entity;
+
+
+
+namespace DataBaseLayer.Context
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>(entity =>
+            {
+
+                entity.HasKey(u => u.UserId);
+
+
+                entity.Property(u => u.UserId)
+                      .ValueGeneratedOnAdd();
+
+
+                entity.Property(u => u.FirstName)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+
+                entity.Property(u => u.LastName)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+
+                entity.Property(u => u.Email)
+                      .IsRequired()
+                      .HasMaxLength(255);
+
+                entity.HasIndex(u => u.Email)
+                      .IsUnique();
+
+
+                entity.Property(u => u.Password)
+                      .IsRequired();
+
+
+                entity.Property(u => u.CreatedAt)
+                      .IsRequired();
+
+
+                entity.Property(u => u.ChangedAt);
+            });
+        }
+
+
+
+    }
+}
---
+++ ModelLayer/Entity/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Entity
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+        public string Password { get; set; }
+
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+        public DateTime ChangedAt { get; set; } = DateTime.UtcNow;
+    }
+}
