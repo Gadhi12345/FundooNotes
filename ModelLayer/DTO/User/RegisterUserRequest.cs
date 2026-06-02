782df6827626a3a10e830b32157ee0b4813cc8a9
Adarsh <adarshniharika20@gmail.com>
2026-06-02 16:05:56 +0530

    In Model Layer created a DTO folder which consists of UserRequest and User Response

---
+++ ModelLayer/DTO/User/RegisterUserRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.User
+{
+    public class RegisterUserRequest
+    {
+
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$",
+     ErrorMessage = "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, and one number.")]
+        public string Password { get; set; }
+    }
+}
---
+++ ModelLayer/DTO/User/UserResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.User
+{
+    //outgoing 
+    public class UserResponse
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
