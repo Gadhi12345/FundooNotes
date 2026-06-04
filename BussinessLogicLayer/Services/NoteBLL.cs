acab216c20f1e6d0b9dc85061eb1fb47ef04af68
Adarsh <adarshniharika20@gmail.com>
2026-06-04 16:48:00 +0530

    Updated Bussiness logic layer with INoteBLL,NoteBLL

---
+++ BussinessLogicLayer/Services/NoteBLL.cs
@@ -0,0 +1,27 @@
+﻿using BussinessLogicLayer.Interface;
+using DataBaseLayer.Context;
+using DataBaseLayer.Interface;
+using ModelLayer.DTO.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicLayer.Services
+{
+    public class NoteBLL : INoteBLL
+    {
+        private readonly INoteDAL _noteDAL;
+
+        public NoteBLL(INoteDAL noteDAL)
+        {
+            _noteDAL = noteDAL;
+        }
+
+        public NoteResponse CreateNote(CreateNoteRequest createNoteRequest, int userId)
+        {
+            return _noteDAL.CreateNote(createNoteRequest, userId);
+        }
+    }
+}
