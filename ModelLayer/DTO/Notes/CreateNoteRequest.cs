da2406433551bca25d0ce99cc42d8dbf51d7594a
Adarsh <adarshniharika20@gmail.com>
2026-06-04 15:44:37 +0530

    Created Notes DTO for Notes Table and Updated the Model Layer is Updated

---
+++ ModelLayer/DTO/Notes/CreateNoteRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.Notes
+{
+    //Incoming Layer
+    public class CreateNoteRequest
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? Reminder { get; set; }
+
+        public string Colour { get; set; }
+
+        public string Image { get; set; }
+    }
+}
