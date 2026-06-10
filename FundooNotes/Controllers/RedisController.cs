4bba4e286abb7cd48c24b537a47a10a42b9da707
Adarsh <adarshniharika20@gmail.com>
2026-06-10 19:28:44 +0530

    Updated Dev Branch

--- FundooNotes/Controllers/RedisController.cs
+++ FundooNotes/Controllers/RedisController.cs
@@ -1,33 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using StackExchange.Redis;
-
-namespace FundooNotes.Controllers
+﻿namespace FundooNotes.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RedisController : ControllerBase
+    public class RedisController
     {
-        private readonly IDatabase _db;
-
-        public RedisController(IConnectionMultiplexer redis)
-        {
-            _db = redis.GetDatabase();
-        }
-
-        [HttpPost("set")]
-        public async Task<IActionResult> Set(string key, string value)
-        {
-            await _db.StringSetAsync(key, value);
-
-            return Ok("Data stored in Redis");
-        }
-
-        [HttpGet("get")]
-        public async Task<IActionResult> Get(string key)
-        {
-            var value = await _db.StringGetAsync(key);
-
-            return Ok(value.ToString());
-        }
     }
 }
